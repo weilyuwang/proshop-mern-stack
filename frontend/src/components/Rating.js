260c948b672d61a47d6679e2f7ef3224ddfa31ff
weilyuwang <weilyuwang@gmail.com>
2020-10-05 23:16:38 -0400

    :zap: rating component

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,85 @@
+import React from "react";
+import PropTypes from "prop-types"; // type impt to import proptypes
+
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className="rating">
+      <span>
+        <i
+          style={{ color: color }}
+          className={
+            value >= 1
+              ? "fas fa-star"
+              : value >= 0.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color: color }}
+          className={
+            value >= 2
+              ? "fas fa-star"
+              : value >= 1.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color: color }}
+          className={
+            value >= 3
+              ? "fas fa-star"
+              : value >= 2.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color: color }}
+          className={
+            value >= 4
+              ? "fas fa-star"
+              : value >= 3.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color: color }}
+          className={
+            value >= 5
+              ? "fas fa-star"
+              : value >= 4.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+
+      {/* if text exists, show it */}
+      <span>{text && text}</span>
+    </div>
+  );
+};
+
+// Set default color prop value
+Rating.defaultProps = {
+  color: "#f8e825",
+};
+
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+};
+
+export default Rating;
