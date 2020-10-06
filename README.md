1cb204502aab062399543256fb0c1887e35e600a
weilyuwang <weilyuwang@gmail.com>
2020-10-06 11:15:02 -0400

    :ambulance: react-router

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 ## React Frontend
 
-Theme: `Bootswatch - LUX` + `react-bootstrap` npm module  
-Icons: `font-awesome` from cdnjs.com
+### Theme
+
+`Bootswatch - LUX` + `react-bootstrap` npm module
+
+### Icons
+
+`font-awesome` from cdnjs.com
+
+### Routes
+
+`react-router-dom` + `react-router-bootstrap`: Integration between React Router v4 and React Bootstrap.
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom"; // with history api integrated
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       {/* py-3: padding on y-axis of value 3 (top & bottom) */}
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
