9be913357fe791f9cada4e74a294111eaa86631a
weilyuwang <weilyuwang@gmail.com>
2020-10-13 19:59:55 -0400

    :fire: add redux

---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// Too use redux dev tools you need:
+// 1. redux-devtools-extension in react app
+// 2. redux dev tools in chrome extension.
+const reducer = combineReducers({});
+const initialState = {};
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
