a3642a0234de572ebc933737ebca5bc715541166
weilyuwang <weilyuwang@gmail.com>
2020-11-04 14:54:06 -0500

    :bug: add controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,33 @@
+// Simple middleware for handling exceptions inside of async express routes
+// and passing them to your express error handlers
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+// @desc      Fetch all products
+// @route     GET /api/products
+// @access    Public
+const getProducts = asyncHandler(async (req, res) => {
+  // Returns every product we have in database
+  const products = await Product.find({});
+
+  // convert the list of objects into JSON
+  res.json(products);
+});
+
+// @desc      Fetch single product by id
+// @route     GET /api/products/:id
+// @access    Public
+const getProductById = asyncHandler(async (req, res) => {
+  // Find the product by param id
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    // Will be handled by the custom error handler middleware
+    throw new Error("Product not found");
+  }
+});
+
+export { getProductById, getProducts };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,39 +1,13 @@
 import express from "express";
-import Product from "../models/productModel.js";
-
-// Simple middleware for handling exceptions inside of async express routes
-// and passing them to your express error handlers
-import asyncHandler from "express-async-handler";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-// @desc      Fetch all products
-// @route     GET /api/products
-// @access    Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({}); // Returns every product we have in database
-    res.json(products); // convert the list of objects into JSON
-  })
-);
+router.route("/").get(getProducts);
 
-// @desc      Fetch single product by id
-// @route     GET /api/products/:id
-// @access    Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    // Find the product by param id
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found"); // Will be handled by the custom error handler middleware
-    }
-  })
-);
+router.route("/:id").get(getProductById);
 
 export default router;
