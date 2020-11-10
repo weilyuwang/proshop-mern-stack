fc5b7435aba7fd0ce7597f93f3bfa672d61d147e
weilyuwang <weilyuwang@gmail.com>
2020-11-09 23:36:38 -0500

    :fire: shipping screen and save address

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -36,8 +36,16 @@ const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
+// Fetch user shipping address from local storage
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
   userLogin: { userInfo: userInfoFromStorage },
 };
 
