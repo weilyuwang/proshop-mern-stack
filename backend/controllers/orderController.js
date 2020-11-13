d5063ba07e931941461fea2e72f03baf187abc1a
weilyuwang <weilyuwang@gmail.com>
2020-11-13 11:08:50 -0500

    :zap: get order by id route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -38,4 +38,22 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+// @desc      Get order by IDr
+// @route     GET /api/orders/:id
+// @access    Private
+const getOrderById = asyncHandler(async (req, res) => {
+  // We also need user's info (name and email)
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order Not Found");
+  }
+});
+
+export { addOrderItems, getOrderById };
