57e9d35b4b1d669378f4940e0f7c0854bf135139
weilyuwang <weilyuwang@gmail.com>
2020-11-14 17:33:23 -0500

    :zap: update order to paid endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -56,4 +56,32 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+// @desc      Update order to paid
+// @route     PUT /api/orders/:id/pay
+// @access    Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  // Find the order
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+
+    // The info below comes from paypal
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order Not Found");
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
