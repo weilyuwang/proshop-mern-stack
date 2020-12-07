8b4ae1f13f951f2329384dac61cb9da69e971ad9
weilyuwang <weilyuwang@gmail.com>
2020-12-07 12:31:13 -0500

    :art: get products

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -4,7 +4,6 @@ import {
   CART_REMOVE_ITEM,
   CART_SAVE_SHIPPING_ADDRESS,
   CART_SAVE_PAYMENT_METHOD,
-  CART_RESET,
 } from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
