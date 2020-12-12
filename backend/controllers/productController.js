dd10e442163e9f98e97a0fc4fd9336acc5dc6b2d
weilyuwang <weilyuwang@gmail.com>
2020-12-11 23:32:15 -0500

    :fire: product pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,6 +7,9 @@ import Product from "../models/productModel.js";
 // @route     GET /api/products
 // @access    Public
 const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 2;
+  const page = Number(req.query.pageNumber) || 1;
+
   const keyword = req.query.keyword
     ? {
         name: {
@@ -16,10 +19,12 @@ const getProducts = asyncHandler(async (req, res) => {
       }
     : {};
 
-  const products = await Product.find({ ...keyword });
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
 
-  // convert the list of objects into JSON
-  res.json(products);
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 // @desc      Fetch single product by id
