2736d27c3471771d74c181aebc1d716fd02fea33
weilyuwang <weilyuwang@gmail.com>
2020-12-12 00:21:20 -0500

    :ambulance: product carousel components

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -14,6 +14,8 @@ const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 
+router.get("/top", getTopProducts);
+
 router
   .route("/:id")
   .get(getProductById)
@@ -21,7 +23,5 @@ router
   .put(protect, admin, updateProduct);
 
 router.route("/:id/reviews/").post(protect, createProductReview);
-
-router.get("/top", getTopProducts);
 
 export default router;
